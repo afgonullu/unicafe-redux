6586012742afe4b9cda4d9b714db42571bafdff9
A Faruk Gonullu <af@gonul.lu>
2020-12-10 14:09:52 +0300

    build: 0.6.2 complete exercise 6.2
    feat: functional buttons
    fix: functional reducer
    test: test all functionality
    FSO: end of exercise 6.2

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,39 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
+  bad: 0,
 }
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
-    case 'GOOD':
+    case "GOOD":
+      return {
+        good: state.good + 1,
+        ok: state.ok,
+        bad: state.bad,
+      }
+    case "OK":
+      return {
+        good: state.good,
+        ok: state.ok + 1,
+        bad: state.bad,
+      }
+    case "BAD":
+      return {
+        good: state.good,
+        ok: state.ok,
+        bad: state.bad + 1,
+      }
+    case "ZERO":
+      return {
+        good: 0,
+        ok: 0,
+        bad: 0,
+      }
+    default:
       return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
   }
-  
 }
 
 export default counterReducer
